7272fc0731163c312ab5f5c73f560f771bd64548
Khozhimatov Muzaffar <xjmuzaffar@gmail.com>
2026-06-18 19:35:32 +0900

    init: backend frontend cors and connection in production

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://rnd-music-store.vercel.app", "http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
